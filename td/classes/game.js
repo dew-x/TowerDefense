dba964311634b920435368fd7f6d0310a4866fc1
Dewcs <andreualien@hotmail.com>
2015-06-26 21:25:51 +0200

    formating

--- td/classes/game.js
+++ td/classes/game.js
@@ -8,6 +8,7 @@ function Game(data, canvasID, width, height) {
     this.canvas = document.getElementById(this.canvasID);
     this.ctx = this.canvas.getContext("2d");
     this.inputQueue = [];
+    this.scene = "loading";
 }
 
 Game.prototype.play = function() {
@@ -15,16 +16,16 @@ Game.prototype.play = function() {
 }
 
 Game.prototype.loop = function(time) {
-	// process inputQueue
-	if (this.inputQueue.length!=0) {
-		this.inputQueue=[];
-	}
+    // process inputQueue
+    if (this.inputQueue.length != 0) {
+        this.inputQueue = [];
+    }
 }
 
 Game.prototype._resize = function() {
     this.width = (this.targetWidth <= 0) ? window.innerWidth : this.targetWidth;
     this.height = (this.targetHeight <= 0) ? window.innerHeight : this.targetHeight;
-    console.log("Resize: ",this.width,this.height,window)
+    console.log("Resize: ", this.width, this.height, window)
     this.canvas.width = this.width;
     this.canvas.height = this.height;
     this.ctx.fillRect(0, 0, this.width, this.height);
@@ -33,125 +34,125 @@ Game.prototype._resize = function() {
 }
 
 Game.prototype._mousedown = function(event) {
-	event.preventDefault();
-	e = event || window.event;
+    event.preventDefault();
+    e = event || window.event;
     var target = e.target || e.srcElement;
     rect = target.getBoundingClientRect();
     offsetX = e.clientX - rect.left;
     offsetY = e.clientY - rect.top;
     this.inputQueue.push({
-    	type: "begin",
-    	x: offsetX,
-    	y: offsetY,
-    	id: 1,
+        type: "begin",
+        x: offsetX,
+        y: offsetY,
+        id: 1,
     });
 }
 
 Game.prototype._mousemove = function(event) {
-	e = event || window.event;
+    e = event || window.event;
     var target = e.target || e.srcElement;
     rect = target.getBoundingClientRect();
     offsetX = e.clientX - rect.left;
     offsetY = e.clientY - rect.top;
     this.inputQueue.push({
-    	type: "move",
-    	x: offsetX,
-    	y: offsetY,
-    	id: 1,
+        type: "move",
+        x: offsetX,
+        y: offsetY,
+        id: 1,
     });
 }
 
 Game.prototype._mouseup = function(event) {
-	e = event || window.event;
+    e = event || window.event;
     var target = e.target || e.srcElement;
     rect = target.getBoundingClientRect();
     offsetX = e.clientX - rect.left;
     offsetY = e.clientY - rect.top;
     this.inputQueue.push({
-    	type: "end",
-    	x: offsetX,
-    	y: offsetY,
-    	id: 1,
+        type: "end",
+        x: offsetX,
+        y: offsetY,
+        id: 1,
     });
 }
 
 Game.prototype._touchdown = function(event) {
-	event.preventDefault();
-	var obj=event.changedTouches||event.originalEvent.changedTouches;
-    for (var i=0; i<obj.length; ++i) {
-    	var target = obj[i].target || obj[i].srcElement;
-    	rect = target.getBoundingClientRect();
-        var x = obj[i].pageX-rect.left;
-        var y = obj[i].pageY-rect.top;
-        var id= i;
+    event.preventDefault();
+    var obj = event.changedTouches || event.originalEvent.changedTouches;
+    for (var i = 0; i < obj.length; ++i) {
+        var target = obj[i].target || obj[i].srcElement;
+        rect = target.getBoundingClientRect();
+        var x = obj[i].pageX - rect.left;
+        var y = obj[i].pageY - rect.top;
+        var id = i;
         this.inputQueue.push({
-	    	type: "begin",
-	    	x: x,
-	    	y: y,
-	    	id: id,
-	    });
+            type: "begin",
+            x: x,
+            y: y,
+            id: id,
+        });
     }
 }
 
 Game.prototype._touchmove = function(event) {
-	event.preventDefault();
-    var obj=event.changedTouches||event.originalEvent.changedTouches;
-    for (var i=0; i<obj.length; ++i) {
-    	var target = obj[i].target || obj[i].srcElement;
-    	rect = target.getBoundingClientRect();
-        var x = obj[i].pageX-rect.left;
-        var y = obj[i].pageY-rect.top;
-        var id= i;
+    event.preventDefault();
+    var obj = event.changedTouches || event.originalEvent.changedTouches;
+    for (var i = 0; i < obj.length; ++i) {
+        var target = obj[i].target || obj[i].srcElement;
+        rect = target.getBoundingClientRect();
+        var x = obj[i].pageX - rect.left;
+        var y = obj[i].pageY - rect.top;
+        var id = i;
         this.inputQueue.push({
-	    	type: "move",
-	    	x: x,
-	    	y: y,
-	    	id: id,
-	    });
+            type: "move",
+            x: x,
+            y: y,
+            id: id,
+        });
     }
 }
 
 Game.prototype._touchup = function(event) {
-	event.preventDefault();
-    var obj=event.changedTouches||event.originalEvent.changedTouches;
-    for (var i=0; i<obj.length; ++i) {
-    	var target = obj[i].target || obj[i].srcElement;
-    	rect = target.getBoundingClientRect();
-        var x = obj[i].pageX-rect.left;
-        var y = obj[i].pageY-rect.top;
-        var id= i;
+    event.preventDefault();
+    var obj = event.changedTouches || event.originalEvent.changedTouches;
+    for (var i = 0; i < obj.length; ++i) {
+        var target = obj[i].target || obj[i].srcElement;
+        rect = target.getBoundingClientRect();
+        var x = obj[i].pageX - rect.left;
+        var y = obj[i].pageY - rect.top;
+        var id = i;
         this.inputQueue.push({
-	    	type: "end",
-	    	x: x,
-	    	y: y,
-	    	id: id,
-	    });
+            type: "end",
+            x: x,
+            y: y,
+            id: id,
+        });
     }
 }
 
 
 Game.prototype._mousewheel = function(event) {
-	event.preventDefault();
-	var e = event || window.event; // old IE support
-	var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-	this.inputQueue.push({
-    	type: "wheel",
-    	delta: delta,
+    event.preventDefault();
+    var e = event || window.event; // old IE support
+    var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+    this.inputQueue.push({
+        type: "wheel",
+        delta: delta,
     });
 }
 
 Game.prototype._keydown = function(event) {
-	var key = event.keyCode ? event.keyCode : event.which;
-	this.inputQueue.push({
-    	type: "keyDown",
-    	key: key,
+    var key = event.keyCode ? event.keyCode : event.which;
+    this.inputQueue.push({
+        type: "keyDown",
+        key: key,
     });
 }
 
 Game.prototype._keyup = function(event) {
-	var key = event.keyCode ? event.keyCode : event.which;
+    var key = event.keyCode ? event.keyCode : event.which;
     this.inputQueue.push({
-    	type: "keyUp",
-    	key: key,
+        type: "keyUp",
+        key: key,
     });
 }
